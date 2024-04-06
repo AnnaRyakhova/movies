53499d774b5ec30a1f6248931821c99ab3a8e768
Ryakhova Anna <anna-muzychuk@mail.ru>
2024-04-06 18:25:10 +0300

    Change MainPage

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,11 +1,32 @@
-import { Link } from 'react-router-dom'
+import { Typography, Divider, Pagination } from 'antd'
+
 import styles from './MainPage.module.css'
+import { MovieCard } from '../../components/FilmCard/MovieCard'
+
+const { Title } = Typography
 
 export const MainPage = () => {
   return (
-    <div>
-      <h1>Главная</h1>
-      <Link to="/movie/1">К фильму</Link>
+    <div className={styles.background}>
+      <div className={styles.root}>
+        <div className={styles.wrapper}>
+          <div className={styles.title}>
+            <Title style={{ color: '#C5C5C5' }}>Авито.Кино</Title>
+            <Title level={3}>Все фильмы и сериалы</Title>
+          </div>
+          <Divider />
+          <div className={styles.content}>
+            <div className={styles.sider}>fff</div>
+            <div className={styles.movies}>
+              <MovieCard />
+              <MovieCard />
+              <MovieCard />
+            </div>
+          </div>
+          <Divider />
+          <Pagination defaultCurrent={1} total={500} className={styles.pagination} />
+        </div>
+      </div>
     </div>
   )
 }
