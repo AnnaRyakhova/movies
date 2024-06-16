225c72336e27f2546d0e097406f256d455cd9925
Ryakhova Anna <anna-muzychuk@mail.ru>
2024-06-16 15:17:37 +0300

    Redesign movie list

---
+++ src/components/MovieCardRedesign/MovieCard.tsx
@@ -0,0 +1,78 @@
+import { FC } from 'react'
+import { Link } from 'react-router-dom'
+import { Skeleton, Typography } from 'antd'
+
+import { Movie } from 'src/types'
+
+import styles from './MovieCard.module.css'
+
+const { Title, Text } = Typography
+
+interface MovieCardProps {
+  movie: Movie
+}
+
+export const MovieCard: FC<MovieCardProps> = ({ movie }) => {
+  const { name, alternativeName, year, movieLength, genres, countries, poster, rating, id } = movie || {}
+
+  const [genreObj] = genres || []
+  const genre = genreObj?.name ? `${genreObj.name}, ` : ''
+
+  const [country] = countries || []
+  const countryName = country?.name ? `${country.name}` : ''
+
+  const anotherName = alternativeName ? `${alternativeName}, ` : ''
+
+  const length = movieLength ? `${movieLength} мин, ` : ''
+
+  const info = `${anotherName}${year}, ${length} ${genre} ${countryName}`
+
+  const movieRating = rating.kp.toFixed(1)
+
+  const renderPoster = () => {
+    if (!poster?.previewUrl) {
+      return <Skeleton.Image className={styles.posterSkeleton} />
+    }
+
+    return (
+      <div className={styles.poster} style={{ backgroundImage: `url(${poster?.previewUrl})` }}>
+        <div className={styles.rating}>{movieRating}</div>
+      </div>
+    )
+  }
+
+  return (
+    <>
+      {/* <Link to={`/movie/${id}`} className={styles.link}> */}
+      <div className={styles.wrapper}>
+        {renderPoster()}
+        <div className={styles.name}>
+          <Title level={4} className={styles.title} style={{ marginBottom: '0' }}>
+            {name}
+          </Title>
+          <Text type="secondary" className={styles.engTitle}>
+            {alternativeName}
+          </Text>
+        </div>
+      </div>
+      {/* <div className={styles.wrapper}>
+          {renderPoster()}
+          <div className={styles.description}>
+            <div className={styles.text}>
+              <Title level={4} className={styles.title} style={{ marginBottom: '0' }}>
+                {name}
+              </Title>
+              <Text type="secondary" className={styles.info}>
+                {info}
+              </Text>
+            </div>
+
+            <Title level={2} className={styles.rating}>
+              {movieRating}
+            </Title>
+          </div>
+        </div> */}
+      {/* </Link> */}
+    </>
+  )
+}
--- src/pages/MoviesListPage/MoviesListPageRedesign.tsx
+++ src/pages/MoviesListPage/MoviesListPageRedesign.tsx
@@ -9,6 +9,42 @@ import { useSearchParams } from 'src/utils/useSearchParams'
 
 import styles from './MoviesListPageRedesign.module.css'
 
+const movie = {
+  id: 666,
+  externalId: {
+    kpHD: '48e8d0acb0f62d8585101798eaeceec5',
+    imdb: 'tt0232500',
+    tmdb: 9799,
+  },
+  name: 'Человек паук',
+  alternativeName: 'Spider man',
+
+  year: 2023,
+  description: 'string',
+  shortDescription: 'string',
+
+  rating: {
+    kp: 6.2,
+  },
+
+  movieLength: 120,
+  ageRating: 16,
+  poster: {
+    url: 'https://st.kp.yandex.net/images/actor_iphone/iphone360_6317.jpg',
+    previewUrl: 'https://st.kp.yandex.net/images/actor_iphone/iphone360_6317.jpg',
+  },
+  genres: [
+    {
+      name: 'string',
+    },
+  ],
+  countries: [
+    {
+      name: 'string',
+    },
+  ],
+}
+
 export const MoviesListPage = () => {
   const [movies, setMovies] = useState<Movie[]>([])
   const [loading, setLoading] = useState(false)
@@ -42,8 +78,22 @@ export const MoviesListPage = () => {
 
         <div className={styles.content}>
           <Filters setLoading={setLoading} setTotal={setTotal} setMovies={setMovies} />
-          <div className={styles.films}>mdkghrlkghlek</div>
+          <div className={styles.films}>
+            <MovieCard movie={movie}></MovieCard>
+            <MovieCard movie={movie}></MovieCard>
+            <MovieCard movie={movie}></MovieCard>
+            <MovieCard movie={movie}></MovieCard>
+            <MovieCard movie={movie}></MovieCard>
+            <MovieCard movie={movie}></MovieCard>
+            <MovieCard movie={movie}></MovieCard>
+            <MovieCard movie={movie}></MovieCard>
+            <MovieCard movie={movie}></MovieCard>
+            <MovieCard movie={movie}></MovieCard>
+            <MovieCard movie={movie}></MovieCard>
+            <MovieCard movie={movie}></MovieCard>
+          </div>
           {/* <div className={styles.movies}>{renderMovies()}</div> */}
+
           <div className={styles.about}>{about}</div>
         </div>
 
